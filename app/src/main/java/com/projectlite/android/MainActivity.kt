04d95492cefb67906dd72fdc55fe1e396f5b0f7a
Linwc <7461827@qq.com>
2020-10-31 02:10:34 +0800

    Revert "搭配国际版LeanCloud，在MyApplication初始化，实现按下按钮向云数据库插入一条“Hello,world!”记录。"
    This reverts commit 0d0752ec2208da5d0e4ea70b92bec02d8d75bd0a.

--- app/src/main/java/com/projectlite/android/MainActivity.kt
+++ app/src/main/java/com/projectlite/android/MainActivity.kt
@@ -1,31 +1,11 @@
 package com.projectlite.android
 
-import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import cn.leancloud.AVObject
-import kotlinx.android.synthetic.main.activity_main.*
-
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        button2.setOnClickListener {
-            val testObject = AVObject("TestObject")
-            testObject.put("words", "Hello world!")
-            testObject.saveInBackground().blockingSubscribe()
-            Toast.makeText(this,"Done!",Toast.LENGTH_SHORT).show()
-        }
-
-        button3.setOnClickListener {
-            Toast.makeText(this,"OK!",Toast.LENGTH_SHORT).show()
-
-        }
-
     }
-
 }
