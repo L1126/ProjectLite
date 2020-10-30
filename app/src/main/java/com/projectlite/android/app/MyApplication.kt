da9eda451e1b2c919561c4e42e24109b1435a4b2
Linwc <7461827@qq.com>
2020-10-31 02:24:16 +0800

    LeanCloud Test.

--- app/src/main/java/com/projectlite/android/app/MyApplication.kt
+++ app/src/main/java/com/projectlite/android/app/MyApplication.kt
@@ -1,16 +1,22 @@
 package com.projectlite.android.app
 
 import android.app.Application
+import cn.leancloud.AVLogger
 import cn.leancloud.AVOSCloud
 
 class MyApplication :Application(){
     override fun onCreate() {
         super.onCreate()
-        // 提供 this、App ID、绑定的自定义 API 域名作为参数
-        AVOSCloud.initializeSecurely(
-            this,
-            "w1vGF0c7QirnF5rFE0CXXFvs-gzGzoHsz",
-            "https://please-replace-with-your-customized.domain.com"
-        )
+
+        // 开启调试日志：在 AVOSCloud.initialize() 之前调用
+        AVOSCloud.setLogLevel(AVLogger.Level.DEBUG);
+        //在应用发布之前，请关闭调试日志，以免暴露敏感数据。
+
+        // 初始化：提供 this、App ID、绑定的自定义 API 域名作为参数
+        AVOSCloud.initialize(
+                this,
+                "xCPMuwMtvrHTaRYE3sdnlBez-MdYXbMMI",
+                "nJY5JvhpKI3Nkcob3IevMNr3"
+        );
     }
 }
